ff4fac589d498d1ea0ade92ab76eeb94aa7667f7
Konstantin Stepanov <688stakan@gmail.com>
2022-01-31 12:21:05 +0300

    разложение числа на множители и подсчет их

---
+++ Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Разложите натуральное число n на простые множители и определите
+//    сколько раз встречается множитель 2 в произведении
+//    наименьший множитель произведения
+//    сколько различных множителей в произведении
+
+int n = new Random().Next(90,91);
+Console.WriteLine(n);
+Console.WriteLine($"наименьший делитель {Min(n)}");
+int count2 = 0;
+while (n%2 == 0)
+{
+    n = n/2;
+    count2++;
+}
+if (count2>0) Console.WriteLine($"множитель 2 встречается {count2}");
+
+int delitel = 3;
+int count = 0;
+while (n>1)
+{
+    while (n%delitel == 0)
+    {
+        n= n/delitel;
+        if (n%delitel!=0)
+            count++;
+    }
+    delitel++;
+}
+
+
+if (count2>0) Console.WriteLine($"колличество уникальных множителей = {count +1}");
+else Console.WriteLine($"колличество уникальных множителей = {count}");
+
+int Min(int n)
+{
+    int mindiv = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            mindiv = i;
+            return mindiv;
+        }
+    }
+    return mindiv;
+}
+
